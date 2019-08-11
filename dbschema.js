70e6d4ba3a64493c9daa92762bf3726b1f8e0f3e
stevelikeafox <stephenwillmot@googlemail.com>
2019-08-11 05:45:57 +0100

    inital commit

--- dbschema.js
+++ dbschema.js
@@ -2,7 +2,7 @@ let db = {
   posts: [
     {
       userHandle: "user",
-      body: "This is the post scream",
+      body: "This is the post",
       createdAt: "2019-07-02T22:23:57.179Z",
       likeCount: 5,
       commentCount: 2
